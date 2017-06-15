42b35dcb9b865dcbbaabae645e661d52c71b97a1
mikeporter <porter.michael.james@gmail.com>
2017-06-15 15:26:17 -0700

    adding scaffold plan

---
+++ bitmap03-3amigos/lib/file-ops.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// read and write bitmap into memory
+//  read and slice buffer buf.readUnit16/8/24LE
+//  call bitmap methods to overwrite write
+
+// buffer, save as property, change buffer, save buffer
---
+++ bitmap03-3amigos/model/bitmap.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//  constructor for bitmap. bitmap.js methods for transforming color array.
+// * Your project should have three transforms
+//   * Invert the colors (essentially subtract every color value from the max color value which is 255),
+//   * Grayscale the colors - Google it.
+//   * (red|green|blue)scale the colors, same as above but only multiply one of the colors.
