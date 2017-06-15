29634cfba7cad2e03bbbf20b2baa5a9941a65352
mikeporter <porter.michael.james@gmail.com>
2017-06-15 16:29:07 -0700

    have a plan

--- bitmap03-3amigos/lib/file-ops.js
+++ bitmap03-3amigos/lib/file-ops.js
@@ -1,7 +1,26 @@
 'use strict';
 
 // read and write bitmap into memory
-//  read and slice buffer buf.readUnit16/8/24LE
+//  read and slice buffer buf.readUnit8/16/32LE
 //  call bitmap methods to overwrite write
 
 // buffer, save as property, change buffer, save buffer
+
+const fileOps = module.exports = {};
+const fs = require('fs');
+
+fileOps.read = () => {
+  // open file using fs and read it into a buffer
+  fs.readFile(path, (err, data) => {
+    if (err)
+    // do something with err
+      return;
+    // do soemthing with data/buffer
+  })
+};
+
+fileOps.write = () => {
+  // writeUint8 stuff to overwrite it
+  //  call bitmap.js functions to change the file.
+
+};
--- bitmap03-3amigos/model/bitmap.js
+++ bitmap03-3amigos/model/bitmap.js
@@ -2,6 +2,23 @@
 
 //  constructor for bitmap. bitmap.js methods for transforming color array.
 // * Your project should have three transforms
+
+
+const fileOps = require('../lib/file-ops.js');
+
+// constructor function to convert buffer headers data into a Javascript Object (using constructors)
+
+function Bitmap(buffer) {
+  this.bitmapFileHeader = buf.slice(0, 14)
+  // based on header data slice these other things
+  // read the 2 bits we sliced with readun16le and .tostring it
+  // read the file with readUint32le
+  // read header data to put dibheader and color table where we want
+  this.dibHeader = buffer.slice the parts we want
+  this.colorTable = buffer.slice the parts we want
+  this.pixelArray = buffer.slice the parts we want
+}
+
 //   * Invert the colors (essentially subtract every color value from the max color value which is 255),
 //   * Grayscale the colors - Google it.
 //   * (red|green|blue)scale the colors, same as above but only multiply one of the colors.
